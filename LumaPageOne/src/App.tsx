0fc2ff0d2293a927bf9d274c482461491d88760c
João Carlos <juniorbezerra1999@hotmail.com>
2025-05-17 18:33:33 -0300

    Melhoria nas rotas do app.tsx adicionando estilos a tela de login novamente

--- LumaPageOne/src/App.tsx
+++ LumaPageOne/src/App.tsx
@@ -1,4 +1,3 @@
-// src/main.tsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { DefaultLayout } from "./components/MainLayout/DefaultLayout";
 import { Home } from "./pages/Home";
@@ -6,32 +5,38 @@ import { PontoPage } from "./pages/Ponto/PontoPage";
 import { RegistrarPonto } from "./pages/Ponto/RegistrarPonto";
 import { LoginSignUp } from "./pages/LoginSignUp";
 import { CadastroSignUp } from "./pages/CadastroSignUp";
+import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginSignUp onLogin={() => {}} />} />
-        <Route
-          path="/cadastro"
-          element={<CadastroSignUp onRegister={() => {}} />}
-        />
-
-        <Route path="/app" element={<DefaultLayout />}>
-          <Route index element={<Navigate to="inicio" replace />} />
-
-          <Route path="inicio" element={<Home />} />
-
-          <Route path="ponto" element={<PontoPage />} />
-
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginSignUp onLogin={() => {}} />} />
           <Route
-            path="ponto/registrar"
-            element={<RegistrarPonto userId="string" />}
+            path="/cadastro"
+            element={<CadastroSignUp onRegister={() => {}} />}
           />
-        </Route>
 
-        <Route path="*" element={<Navigate to="/app/inicio" replace />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/app" element={<DefaultLayout />}>
+            <Route index element={<Navigate to="inicio" replace />} />
+
+            <Route path="inicio" element={<Home />} />
+
+            <Route path="ponto" element={<PontoPage />} />
+
+            <Route
+              path="ponto/registrar"
+              element={<RegistrarPonto userId="string" />}
+            />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/app/inicio" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
